9fd3c8cf3b8d51619c21ed17186b62229624dee6
Adria Lopez <adria.lopez.ortega@gmail.com>
2016-06-09 15:58:33 +0200

    Calling other gulp modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+var del = require('del');
+var minify = require('gulp-minify');
+var del = require('del');
+var modify = require('gulp-modify');
 
 var filesToMove = [
     './src/**/*.css',
@@ -15,9 +19,24 @@ gulp.task('default', ['copyJs'], function () {
     // the base option sets the relative root for the set of files,
     // preserving the folder structure
     return gulp.src(filesToMove, { base: './' })
+        .pipe(modify({
+            fileModifier: function (file, contents) {
+                var fileName = file.history.toString();
+                if (fileName.endsWith('index.html')) {
+                    return contents.split(".js\"").join("-min.js\"");
+                }
+                return contents;
+            }
+        }))
         .pipe(gulp.dest('build/Release'));
 });
-gulp.task('copyJs', function () {
+gulp.task('copyJs', ['cleanReleaseFolder'], function () {
     return gulp.src(jsToCopy, { base: './' })
+        .pipe(minify({
+            noSource: true
+        }))
         .pipe(gulp.dest('build/Release'));
+});
+gulp.task('cleanReleaseFolder', function () {
+    return del.sync('./build/Release/**');
 });
