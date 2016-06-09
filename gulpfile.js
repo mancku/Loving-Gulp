ce3d5fb2ffee22482b31241958cdba18a32ba24d
Adria Lopez <adria.lopez.ortega@gmail.com>
2016-06-09 13:43:45 +0200

    Calling other tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 var gulp = require('gulp');
+var minify = require('gulp-minify');
 
 var filesToMove = [
     './src/**/*.css',
+    './index.html',
+    './learn.json',
+];
+var jsToCopy = [
     './src/**/*.js',
     './js/**/*.js',
-    './index.html',
-    './learn.json'
 ];
 
 
-gulp.task('default', function () {
-        // the base option sets the relative root for the set of files,
+gulp.task('default', ['copyJs'], function () {
+    // the base option sets the relative root for the set of files,
     // preserving the folder structure
     return gulp.src(filesToMove, { base: './' })
+        .pipe(gulp.dest('build/Release'));
+});
+gulp.task('copyJs', function () {
+    return gulp.src(jsToCopy, { base: './' })
         .pipe(gulp.dest('build/Release'));
 });
