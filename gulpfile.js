6fbf1aa2ba4befd57b7ee7b43148437f9e2a8e27
Adria Lopez <adria.lopez.ortega@gmail.com>
2016-06-09 13:14:39 +0200

    Adding functionality to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
 
+var filesToMove = [
+    './src/**/*.css',
+    './src/**/*.js',
+    './js/**/*.js',
+    './index.html',
+    './learn.json'
+];
+
+
 gulp.task('default', function () {
-    console.log('Gulp is here!!');
+        // the base option sets the relative root for the set of files,
+    // preserving the folder structure
+    return gulp.src(filesToMove, { base: './' })
+        .pipe(gulp.dest('build/Release'));
 });
